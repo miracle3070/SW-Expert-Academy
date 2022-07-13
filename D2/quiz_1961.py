ba17115d4429ebeba78a837f05462b50fa141e02
miracle3070 <miracle3070@naver.com>
2022-07-13 23:18:21 +0900

    1961. 숫자 배열 회전 (D2) - 미해결

--- D2/quiz_1961.py
+++ D2/quiz_1961.py
@@ -2,9 +2,14 @@
 # 메모: 문제(예제)를 이해하지 못함.
 
 # 2022-07-11 메모
-# 미해경
+# 미해결
 # 풀이시간: 67분
 # 오류 계속 발생
+
+# 2022-07-13 메모
+# 미해결
+# 풀이시간: 60분
+# 테스트케이스 10중 5개 실패
 
 import sys
 sys.stdin = open("input.txt", "r")
@@ -16,33 +21,37 @@ for test_case in range(T):
     for _ in range(n):
         row = list(map(int, input().split()))
         matrix.append(row)
+    result = [[] for _ in range(n)]
 
-    t1 = []
-    for i in range(3):
-        for k in range(n//2):
-            for a in range(n-k-1, -1, -1):
-                t1.append(matrix[a][k])
-            t2 = matrix[k][k:n-k]
-            matrix[k][k:n-k] = list(t1)
-
-            t1 = []
-            for a in range(n-k):
-                t1.append(matrix[a][n-k-1])
-
-            try:
-                for a in range(n-k):
-                    matrix[a][n-k-1] = t2[a]
-            except IndexError:
-                print("에러발생")
-                print("n=", n)
-                print(a, n-k-1)
-
+    for _ in range(3):
+        for k in range(n // 2):
+            t1 = matrix[k][k:n-k]
             t2 = []
-            for a in range(n-k-1, -1, -1):
-                t2.append(matrix[n-k-1][a])
-            matrix[n-k-1][k:n-k] = list(t1)
+            for i in range(k, n-k):
+                t2.append(matrix[i][n-k-1])
+            t3 = matrix[n-k-1][k:n-k]
+            t3 = t3[::-1]
+            t4 = []
+            for i in range(n-k-1, (k-1), -1):
+                t4.append(matrix[i][k])
 
-            for a in range(n-k):
-                matrix[a][k] = t2[a]
-            t1 = []
+            idx = 0
+            for i in range(k, n-k):
+                matrix[k][i] = t4[idx]
+                matrix[i][n-k-1] = t1[idx]
+                matrix[n-k-1][i] = t2[idx]
+                matrix[i][k] = t3[idx]
+                idx += 1
+
+            matrix[k][k] = t4[0]
+            matrix[k][n-k-1] = t1[0]
+            matrix[n-k-1][n-k-1] = t2[0]
+            matrix[n-k-1][k] = t3[0]
+
+        for i in range(n):
+            result[i].append("".join(map(str, matrix[i])))
+
+    print(f"#{test_case+1}")
+    for r in result:
+        print(" ".join(r))
 
