51f8c027aff23def138f041ad7b13e27ee0dbb15
miracle3070 <miracle3070@naver.com>
2022-07-14 21:30:08 +0900

    1961. 숫자 배열 회전 (D2) - 해결

--- D2/quiz_1961.py
+++ D2/quiz_1961.py
@@ -11,6 +11,11 @@
 # 풀이시간: 60분
 # 테스트케이스 10중 5개 실패
 
+# 2022-07-14 메모
+# 해결완료
+# 풀이시간: 30분
+# 명관이에게서 아이디어를 얻음.
+
 import sys
 sys.stdin = open("input.txt", "r")
 
@@ -19,39 +24,24 @@ for test_case in range(T):
     n = int(input())
     matrix = []
     for _ in range(n):
-        row = list(map(int, input().split()))
+        row = input().split()
         matrix.append(row)
     result = [[] for _ in range(n)]
 
     for _ in range(3):
-        for k in range(n // 2):
-            t1 = matrix[k][k:n-k]
-            t2 = []
-            for i in range(k, n-k):
-                t2.append(matrix[i][n-k-1])
-            t3 = matrix[n-k-1][k:n-k]
-            t3 = t3[::-1]
-            t4 = []
-            for i in range(n-k-1, (k-1), -1):
-                t4.append(matrix[i][k])
-
-            idx = 0
-            for i in range(k, n-k):
-                matrix[k][i] = t4[idx]
-                matrix[i][n-k-1] = t1[idx]
-                matrix[n-k-1][i] = t2[idx]
-                matrix[i][k] = t3[idx]
-                idx += 1
-
-            matrix[k][k] = t4[0]
-            matrix[k][n-k-1] = t1[0]
-            matrix[n-k-1][n-k-1] = t2[0]
-            matrix[n-k-1][k] = t3[0]
-
+        switched_matrix = []
         for i in range(n):
-            result[i].append("".join(map(str, matrix[i])))
+            temp = []
+            for k in range(n-1, -1, -1):
+                temp.append(matrix[k][i])
+            switched_matrix.append(temp)
+        for i in range(n):
+            result[i].append(switched_matrix[i])
+        matrix = switched_matrix
 
     print(f"#{test_case+1}")
-    for r in result:
-        print(" ".join(r))
+    for row in result:
+        for r in row:
+            print("".join(r), end=" ")
+        print()
 
